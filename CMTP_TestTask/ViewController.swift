ffe4de8b414359475819a45d0e4523d49863f0c8
Daniil <Dan4ikbite@gmail.com>
2020-07-12 12:43:10 +0300

    Initial Commit

---
+++ CMTP_TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CMTP_TestTask
+//
+//  Created by Dan on 12.07.2020.
+//  Copyright © 2020 Daniil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
